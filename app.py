0cc39d7bd7597a7cdbc25f56a65aca84e64e65e6
eopeedroo <clovisgamersbr@gmail.com>
2026-05-02 16:53:10 -0300

    Add complete Flask application code

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask application!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
